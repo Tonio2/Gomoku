47b38e133d44867b9731b369b5082159fc7e23c7
Antoine Labalette <labalette.antoine@gmail.com>
2024-03-05 12:35:19 +0100

    feat: return list of all evaluated moves for debug purposes

--- GomokuEngine/AI1.cpp
+++ GomokuEngine/AI1.cpp
@@ -5,76 +5,137 @@ GomokuAI::GomokuAI(GomokuGame game, Player ai_player, int depth) : game(game), a
     human_player = (ai_player == X) ? O : X;
 }
 
-std::pair<int, std::pair<int, int>> GomokuAI::minimax(int depth, int alpha, int beta, bool maximizingPlayer)
-{
-    std::pair<int, int> bestMove = {-1, -1};
-    if (depth == 0 || game.is_game_over())
-    {
-        return {heuristic_evaluation(), bestMove};
+MoveEvaluation GomokuAI::minimax(int depth, int alpha, int beta, bool maximizingPlayer) {
+    MoveEvaluation node;
+    node.move = {-1, -1};  // Initialize with an invalid move.
+    if (depth == 0 || game.is_game_over()) {
+        node.score = heuristic_evaluation();
+        return node;
     }
 
-    if (maximizingPlayer)
-    {
+    if (maximizingPlayer) {
         int maxEval = std::numeric_limits<int>::min();
-        for (int i = 0; i < game.get_board_size(); i++)
-        {
-            for (int j = 0; j < game.get_board_size(); j++)
-            {
-                if (game.get_board_value(i, j) == E)
-                {
-                    game.make_move(i, j); // Remplacer par is_legal to prevent capture
-                    auto result = minimax(depth - 1, alpha, beta, false);
-                    int eval = result.first;
-                    game.set_board_value(i, j, E);
-
-                    if (eval > maxEval)
-                    {
-                        maxEval = eval;
-                        bestMove = {i, j};
-                        alpha = std::max(alpha, eval);
-                    }
-
-                    if (beta <= alpha)
-                    {
-                        std::cout << "Pruning at depth " << depth << std::endl;
-                        break;
-                    }
-                }
-            }
-        }
-        return {maxEval, bestMove};
-    }
-    else
-    {
-        int minEval = std::numeric_limits<int>::max();
-        for (int i = 0; i < game.get_board_size(); i++)
-        {
-            for (int j = 0; j < game.get_board_size(); j++)
-            {
-                if (game.get_board_value(i, j) == E)
-                {
+        for (int i = 0; i < game.get_board_size(); i++) {
+            for (int j = 0; j < game.get_board_size(); j++) {
+                if (game.get_board_value(i, j) == E) {
                     game.make_move(i, j);
-                    auto result = minimax(depth - 1, alpha, beta, true);
-                    int eval = result.first;
+                    MoveEvaluation evalNode = minimax(depth - 1, alpha, beta, false);
                     game.set_board_value(i, j, E);
 
-                    if (eval < minEval)
-                    {
-                        minEval = eval;
-                        beta = std::min(beta, eval);
+                    if (evalNode.score > maxEval) {
+                        maxEval = evalNode.score;
+                        node.move = {i, j};
+                        node.score = maxEval;
+                        alpha = std::max(alpha, evalNode.score);
                     }
 
-                    if (beta <= alpha)
-                    {
-                        std::cout << "Pruning at depth " << depth << std::endl;
+                    node.listMoves.push_back(evalNode);
+
+                    if (beta <= alpha) {
                         break;
                     }
                 }
             }
         }
-        return {minEval, bestMove};
+    } else {
+        int minEval = std::numeric_limits<int>::max();
+        for (int i = 0; i < game.get_board_size(); i++) {
+            for (int j = 0; j < game.get_board_size(); j++) {
+                if (game.get_board_value(i, j) == E) {
+                    game.make_move(i, j);
+                    MoveEvaluation evalNode = minimax(depth - 1, alpha, beta, true);
+                    game.set_board_value(i, j, E);
+
+                    if (evalNode.score < minEval) {
+                        minEval = evalNode.score;
+                        node.move = {i, j};
+                        node.score = minEval;
+                        beta = std::min(beta, evalNode.score);
+                    }
+
+                    node.listMoves.push_back(evalNode);
+
+                    if (beta <= alpha) {
+                        break;
+                    }
+                }
+            }
+        }
     }
+    return node;
 }
+
+
+// std::pair<int, std::pair<int, int>> GomokuAI::minimax(int depth, int alpha, int beta, bool maximizingPlayer)
+// {
+//     std::pair<int, int> bestMove = {-1, -1};
+//     if (depth == 0 || game.is_game_over())
+//     {
+//         return {heuristic_evaluation(), bestMove};
+//     }
+
+//     if (maximizingPlayer)
+//     {
+//         int maxEval = std::numeric_limits<int>::min();
+//         for (int i = 0; i < game.get_board_size(); i++)
+//         {
+//             for (int j = 0; j < game.get_board_size(); j++)
+//             {
+//                 if (game.get_board_value(i, j) == E)
+//                 {
+//                     game.make_move(i, j); // Remplacer par is_legal to prevent capture
+//                     auto result = minimax(depth - 1, alpha, beta, false);
+//                     int eval = result.first;
+//                     game.set_board_value(i, j, E);
+
+//                     if (eval > maxEval)
+//                     {
+//                         maxEval = eval;
+//                         bestMove = {i, j};
+//                         alpha = std::max(alpha, eval);
+//                     }
+
+//                     if (beta <= alpha)
+//                     {
+//                         std::cout << "Pruning at depth " << depth << std::endl;
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+//         return {maxEval, bestMove};
+//     }
+//     else
+//     {
+//         int minEval = std::numeric_limits<int>::max();
+//         for (int i = 0; i < game.get_board_size(); i++)
+//         {
+//             for (int j = 0; j < game.get_board_size(); j++)
+//             {
+//                 if (game.get_board_value(i, j) == E)
+//                 {
+//                     game.make_move(i, j);
+//                     auto result = minimax(depth - 1, alpha, beta, true);
+//                     int eval = result.first;
+//                     game.set_board_value(i, j, E);
+
+//                     if (eval < minEval)
+//                     {
+//                         minEval = eval;
+//                         beta = std::min(beta, eval);
+//                     }
+
+//                     if (beta <= alpha)
+//                     {
+//                         std::cout << "Pruning at depth " << depth << std::endl;
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+//         return {minEval, bestMove};
+//     }
+// }
 
 int GomokuAI::heuristic_evaluation()
 {
@@ -85,10 +146,8 @@ int GomokuAI::heuristic_evaluation()
     return distr(eng); // Generate and return a random number
 }
 
-std::pair<int, int> GomokuAI::suggest_move()
+MoveEvaluation GomokuAI::suggest_move()
 {
-    auto result = minimax(depth, std::numeric_limits<int>::min(), std::numeric_limits<int>::max(), true);
-    std::cout << "Best move is " << result.second.first << ", " << result.second.second << std::endl
-              << "With a score of " << result.first << std::endl;
-    return result.second; // Return the best move
+    MoveEvaluation result = minimax(depth, std::numeric_limits<int>::min(), std::numeric_limits<int>::max(), true);
+    return result;
 }
