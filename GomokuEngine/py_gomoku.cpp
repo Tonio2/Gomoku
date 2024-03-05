8b94a244d2bbc486eced31c1442cba2309a2e5ac
Antoine Labalette <labalette.antoine@gmail.com>
2024-03-05 11:03:47 +0100

    fix: semicolon

--- GomokuEngine/py_gomoku.cpp
+++ GomokuEngine/py_gomoku.cpp
@@ -19,7 +19,7 @@ PYBIND11_MODULE(pygomoku, m)
         .def("count_open_threes", &GomokuGame::count_open_threes)
         .def("get_winner", &GomokuGame::get_winner)
         .def("get_board_value", &GomokuGame::get_board_value)
-        .def("get_board_size", &GomokuGame::get_board_size);
+        .def("get_board_size", &GomokuGame::get_board_size)
         .def("get_current_player", &GomokuGame::get_current_player);
     py::class_<GomokuAI>(m, "GomokuAI")
         .def(py::init<GomokuGame &, Player, int>())
