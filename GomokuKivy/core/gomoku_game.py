762f458106b654bd4c0fe82e401c0ca4adb078da
Arthur MASSON <amasson@student.42.fr>
2024-03-06 13:44:49 +0100

    timing widgets

--- GomokuKivy/core/gomoku_game.py
+++ GomokuKivy/core/gomoku_game.py
@@ -51,12 +51,47 @@ class GomokuGame:
         return GomokuPlayer(self.game.get_current_player())
 
     def play_at(self, row: int, col: int):
+        modified = False
         try:
-            self.game.make_move(row, col)
-            CallbackCenter.shared().send_message("GomokuGame.modified", self)
+            new_move = GomokuMove()
+            new_move.player = self.get_current_player()
+            new_move.row = row
+            new_move.column = col
+            new_move.timestamp = self.game_time
+            
+            new_move.move_result = self.game.make_move(row, col)
+            modified = True
+
+            self.last_move_index += 1
+            self.resize_move_list(self.last_move_index + 1)
+
+            self.move_list[self.last_move_index] = new_move
+
         except:
             print(f'Failed to play')
+        
+        if modified:
+            CallbackCenter.shared().send_message("GomokuGame.modified", self)
 
     def increase_time(self, delta_time: float):
         self.game_time += delta_time
         self.players_time[self.get_current_player()] -= delta_time
+        CallbackCenter.shared().send_message("GomokuGame.time", self)
+
+    def reverse_last_move(self):
+        if (self.last_move_index < 0):
+            return
+
+        self.game.reverse_move(self.move_list[self.last_move_index].move_result)
+        self.last_move_index -= 1
+        CallbackCenter.shared().send_message("GomokuGame.modified", self)
+
+    def reapply_last_move(self):
+        reapply_index = self.last_move_index + 1
+
+        if (reapply_index >= len(self.move_list)):
+            return
+
+        self.game.reapply_move(self.move_list[reapply_index].move_result)
+        self.last_move_index += 1
+        CallbackCenter.shared().send_message("GomokuGame.modified", self)
--- GomokuKivy/widgets/game/game_infos_widget.py
+++ GomokuKivy/widgets/game/game_infos_widget.py
@@ -1,15 +1,64 @@
 from kivy.uix.widget import Widget
 from core.callback_center import CallbackCenter
 from core.gomoku_game import GomokuGame, GomokuPlayer, GomokuMove
+from app.shared_object import SharedObject
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+)
+from kivy.clock import Clock
+
 
 class GameInfosWidget(Widget):
 
+    time_marker_black = ObjectProperty(None)
+    time_marker_white = ObjectProperty(None)
     current_player: GomokuPlayer
 
     def __init__(self, **kwargs):
         super(GameInfosWidget, self).__init__(**kwargs)
         CallbackCenter.shared().add_callback("GomokuGame.modified", self.on_game_modified)
+        CallbackCenter.shared().add_callback("GomokuGame.time", self.on_game_time_modified)
+        Clock.schedule_interval(self.tick_game_time, 1/30)
+
+    def tick_game_time(self, dt):
+        game = self.get_game()
+        
+        if game is None:
+            return
+        
+        game.increase_time(dt)
 
     def on_game_modified(self, message, game: GomokuGame):
         self.current_player = game.get_current_player()
+
+    def on_game_time_modified(self, message, game: GomokuGame):
+        if game is None:
+            self.time_marker_black.text = ""
+            self.time_marker_white.text = ""
+            return
+        
+        def time_to_string(time: float) -> str:
+            return "{:.1f}".format(time)
+
+        black_time = game.players_time[GomokuPlayer.BLACK]
+        self.time_marker_black.text = time_to_string(black_time)
+        white_time = game.players_time[GomokuPlayer.WHITE]
+        self.time_marker_white.text = time_to_string(white_time)
+
+    def get_game(self) -> GomokuGame:
+        return SharedObject.get_instance().get_game()
+
+    def reverse_move(self):
+        game = self.get_game()
+
+        if game is None:
+            return
+        
+        game.reverse_last_move()
+
+    def reapply_move(self):
+        game = self.get_game()
+
+        if game is None:
+            return
         
