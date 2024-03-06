64befc868befdf5c0c7ce5f82a1a2aa55f4a1fba
Arthur MASSON <amasson@student.42.fr>
2024-03-06 11:27:14 +0100

    Pybind MoveResult and CellChange

--- GomokuEngine/py_gomoku.cpp
+++ GomokuEngine/py_gomoku.cpp
@@ -14,5 +14,18 @@ PYBIND11_MODULE(pygomoku, m)
       .def("get_winner", &GomokuGame::get_winner)
       .def("get_board_value", &GomokuGame::get_board_value)
       .def("get_board_size", &GomokuGame::get_board_size)
-      .def("get_current_player", &GomokuGame::get_current_player);
+      .def("get_current_player", &GomokuGame::get_current_player)
+      .def("reverse_move", &GomokuGame::reverse_move)
+      .def("reapply_move", &GomokuGame::reapply_move);
+  py::class_<CellChange>(m, "CellChange")
+      .def(py::init<>())
+      .def_readwrite("row", &CellChange::row)
+      .def_readwrite("col", &CellChange::col)
+      .def_readwrite("old_value", &CellChange::old_value)
+      .def_readwrite("new_value", &CellChange::new_value);
+  py::class_<MoveResult>(m, "MoveResult")
+      .def(py::init<>())
+      .def_readwrite("cell_changes", &MoveResult::cell_changes)
+      .def_readwrite("white_score_change", &MoveResult::white_score_change)
+      .def_readwrite("black_score_change", &MoveResult::black_score_change);
 }
