42d06e5a260117d400b1f02ef7b139a89d5dce0b
Antoine Labalette <labalette.antoine@gmail.com>
2024-03-18 22:22:59 +0100

    feat: extract depth into env

--- GomokuEngine/src/main.cpp
+++ GomokuEngine/src/main.cpp
@@ -8,7 +8,9 @@
 
 #include "AI1.h"
 
-#define DEPTH 4
+#ifndef DEPTH
+#define DEPTH 3 // Default depth value
+#endif
 
 std::vector<char> boardCoordinates = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
 
