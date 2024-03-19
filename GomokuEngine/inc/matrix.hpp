d1ab489cbc08f059795b854ade58cb71e847a5c9
Antoine Labalette <labalette.antoine@gmail.com>
2024-03-19 13:36:45 +0100

    feat: make functions inline in matrix

--- GomokuEngine/inc/matrix.hpp
+++ GomokuEngine/inc/matrix.hpp
@@ -3,12 +3,13 @@
 
 #pragma once
 
-template<typename Element>
-class Matrix {
+template <typename Element>
+class Matrix
+{
 
 public:
-
-    struct Index {
+    struct Index
+    {
         int8_t row;
         int8_t col;
 
@@ -16,33 +17,36 @@ public:
 
         Index(int r, int c) : row(r), col(c) {}
 
-        bool is_valid(const Matrix<Element>& matrix) const {
+        bool is_valid(const Matrix<Element> &matrix) const
+        {
             return matrix.is_in_bound(row, col);
         }
     };
 
     /** Init */
 
-    Matrix() :
-    _width(0), _height(0), _elements(0)
-    {}
+    Matrix() : _width(0), _height(0), _elements(0)
+    {
+    }
 
-    Matrix(int width, int height) :
-    _width(width), _height(height), _elements(width * height)
-    {}
+    Matrix(int width, int height) : _width(width), _height(height), _elements(width * height)
+    {
+    }
 
-    Matrix(const Matrix<Element>& copy) :
-    _width(copy._width), _height(copy._height), _elements(copy._elements)
-    {}
+    Matrix(const Matrix<Element> &copy) : _width(copy._width), _height(copy._height), _elements(copy._elements)
+    {
+    }
 
-    Matrix(Matrix<Element>&& move) noexcept :
-    _width(move._width), _height(move._height), _elements(std::move(move._elements))
-    {}
+    Matrix(Matrix<Element> &&move) noexcept : _width(move._width), _height(move._height), _elements(std::move(move._elements))
+    {
+    }
 
     ~Matrix()
-    {}
+    {
+    }
 
-    Matrix& operator=(const Matrix<Element>& copy) {
+    Matrix &operator=(const Matrix<Element> &copy)
+    {
         if (this != &copy)
         {
             _width = copy._width;
@@ -52,7 +56,8 @@ public:
         return *this;
     }
 
-    Matrix& operator=(Matrix<Element>&& move) noexcept {
+    Matrix &operator=(Matrix<Element> &&move) noexcept
+    {
         if (this != &move)
         {
             _width = move._width;
@@ -64,39 +69,45 @@ public:
 
     /** Element getter */
 
-    Element& operator()(int row, int col) {
+    Element &operator()(int row, int col)
+    {
         return _elements[index_at(row, col)];
     }
 
-    const Element& operator()(int row, int col) const {
+    const Element &operator()(int row, int col) const
+    {
         return _elements[index_at(row, col)];
     }
 
-    Element& operator[](Index index) {
+    Element &operator[](Index index)
+    {
         return _elements[index_at(index.row, index.col)];
     }
 
-    const Element& operator[](Index index) const {
+    const Element &operator[](Index index) const
+    {
         return _elements[index_at(index.row, index.col)];
     }
 
-    bool is_in_bound(int row, int col) const {
-        return row >= 0 && row < _height
-            && col >= 0 && col < _width;
+    inline bool is_in_bound(int row, int col) const
+    {
+        return row >= 0 && row < _height && col >= 0 && col < _width;
     }
 
-    int get_width() const {
+    int get_width() const
+    {
         return _width;
     }
 
-    int get_height() const {
+    int get_height() const
+    {
         return _height;
     }
 
-
     /** Print */
 
-    void print() const {
+    void print() const
+    {
         std::cout << "[" << _width << ";" << _height << "]" << std::endl;
         for (int row = 0; row < _height; ++row)
         {
@@ -109,13 +120,12 @@ public:
     }
 
 private:
-
-    int index_at(int row, int col) const {
+    inline int index_at(int row, int col) const
+    {
         return row * _width + col;
     }
 
     int _width;
     int _height;
     std::vector<Element> _elements;
-
 };
