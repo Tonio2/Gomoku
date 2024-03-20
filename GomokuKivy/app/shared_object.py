c90810533f7c0092c583318bcf847b9fe811a50d
Antoine Labalette <labalette.antoine@gmail.com>
2024-03-20 18:35:13 +0100

    feat: can play againt AI

--- GomokuKivy/app/shared_object.py
+++ GomokuKivy/app/shared_object.py
@@ -5,20 +5,28 @@ class SharedObject:
 
     _width: int
     _height: int
+    _mode: int
     _game: GomokuGame
     _player_time: float
 
     def __init__(self):
         self._player_time = 300
-        self.set_size(19, 19)
+        self._mode = 0
+        self._width = 19
+        self._height = 19
+        self.reset_game()
 
     def set_size(self, width, height):
         self._width = width
         self._height = height
         self.reset_game()
+    
+    def set_mode(self, mode):
+        self._mode = mode
+        self.reset_game()
 
     def reset_game(self):
-        self._game = GomokuGame(self._width, self._height, self._player_time)
+        self._game = GomokuGame(self._width, self._height, self._player_time, self._mode)
 
     def get_game(self):
         return self._game
--- GomokuKivy/gomoku.py
+++ GomokuKivy/gomoku.py
@@ -1,14 +1,35 @@
 from app.gomoku_app import GomokuApp
+from app.shared_object import SharedObject
 
 from core.callback_center import CallbackCenter
-from core.gomoku_game import GomokuGame
+from core.gomoku_game import GomokuGame, GomokuPlayer
+
+def get_game() -> GomokuGame:
+        return SharedObject.get_instance().get_game()
 
 def display_moves(message, game: GomokuGame):
     move_string = ""
     for move in game.move_list:
         move_string += game.coordinates_name(move.row, move.column) + ','
     print(move_string[:-1])
+    
+def get_AI_suggestion(message, game: GomokuGame):
+    gomoku_game = get_game()
+    if gomoku_game is None:
+        return
+    
+    return gomoku_game.get_AI_suggestion()
+
+def handle_end_turn(message, game: GomokuGame):
+    gomoku_game = get_game()
+    if gomoku_game is None:
+        return
+    
+    if gomoku_game.get_current_player() == GomokuPlayer.WHITE:
+        move = get_AI_suggestion(message, game)
+        gomoku_game.play_at(move[0], move[1])
 
 if __name__ == '__main__':
-    # CallbackCenter.shared().add_callback("GomokuGame.modified", display_moves)
+    CallbackCenter.shared().add_callback("GomokuGame.modified", display_moves)
+    CallbackCenter.shared().add_callback("GomokuGame.endturn", handle_end_turn)
     GomokuApp().run()
