35ac6c1ef2f195e7414513dc80131ac9da808770
amasson <giantwow3896@gmail.com>
2024-03-23 12:09:12 +0100

    save and load ai data

--- GomokuEngine/inc/gomoku_ai_data.h
+++ GomokuEngine/inc/gomoku_ai_data.h
@@ -7,27 +7,22 @@ class GomokuAIData
 {
 public:
     GomokuAIData();
-
     GomokuAIData(const GomokuAIData &copy);
-
     GomokuAIData &operator=(const GomokuAIData &copy);
-
     virtual ~GomokuAIData();
 
+    void save_to_file(std::string filename) const;
+    void load_from_file(std::string filename);
+
     float value_of_structure(int structure_type) const;
-
     float value_of_multiple_forced() const;
-
     float value_of_multiple_o4() const;
-
     float value_of_captures(int capture_count) const;
 
 private:
     static const int STC = StructureType::COUNT_STRUCTURE_TYPE;
     static const int VALUES_COUNT = STC + 5;
     float values[VALUES_COUNT];
-
-    void initialize();
 
     friend class GomokuAIDataMutator;
 };
