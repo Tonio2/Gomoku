eb1e692e6d6e02ea74310ccf67a30ee2876e4960
amasson42 <giantwow3896@gmail.com>
2024-04-01 19:28:36 +0200

    main is back as gktool

--- GomokuEngine/src/utils/gomoku_utilities.cpp
+++ GomokuEngine/src/utils/gomoku_utilities.cpp
@@ -1,6 +1,8 @@
 
 #include "utils/gomoku_utilities.h"
 #include <sstream>
+#include <iostream>
+#include <fstream>
 
 int char_to_coordinate(char coordinate)
 {
@@ -55,101 +57,123 @@ void apply_moves(GomokuGame &game, std::string move_str)
     return apply_moves(game, moves);
 }
 
-// void writeMoveEvaluation(std::ofstream &out, const MoveEvaluation &eval, int depth)
-// {
-//     // Create indentation based on the depth
-//     std::string indent(depth * 4, ' '); // 4 spaces for each level of depth
+int get_depth_from_env(int default_depth)
+{
+    char* env_depth = getenv("DEPTH");
 
-//     out << indent << "Move: " << boardCoordinates[eval.move.first] << boardCoordinates[eval.move.second] << " | " << eval.score << "\n";
-//     if (depth < DEPTH)
-//     {
-//         out << indent << "Evals: " << eval.neededEvalCount << " / " << eval.evaluatedMoves << " / " << eval.totalEvalCount << "\n";
-//     }
+    if (env_depth)
+    {
+        int value = atoi(env_depth);
+        if (value != 0)
+            return value;
+    }
+    return default_depth;
+}
 
-//     if (not eval.listMoves.empty())
-//     {
-//         out << indent << "ListMoves:\n";
+void writeMoveEvaluation(std::ofstream &out, const MoveEvaluation &eval, int depth)
+{
+    // Create indentation based on the depth
+    std::string indent(depth * 4, ' '); // 4 spaces for each level of depth
 
-//         for (const auto &move : eval.listMoves)
-//         {
-//             // Increase the indentation for each level
-//             writeMoveEvaluation(out, move, depth + 1);
-//         }
-//     }
-// }
+    out << indent << "Move: " << coordinate_to_char(eval.move.first) << coordinate_to_char(eval.move.second) << " | " << eval.score << "\n";
+    if (depth < get_depth_from_env())
+    {
+        out << indent << "Evals: " << eval.neededEvalCount << " / " << eval.evaluatedMoves << " / " << eval.totalEvalCount << "\n";
+    }
 
-// void logMoveEvaluation(const MoveEvaluation &eval)
-// {
-//     std::ofstream out("log.txt");
-//     if (!out.is_open())
-//     {
-//         std::cerr << "Failed to open log.txt" << std::endl;
-//         return;
-//     }
+    if (not eval.listMoves.empty())
+    {
+        out << indent << "ListMoves:\n";
 
-//     writeMoveEvaluation(out, eval);
-//     out.close();
-// }
+        for (const auto &move : eval.listMoves)
+        {
+            // Increase the indentation for each level
+            writeMoveEvaluation(out, move, depth + 1);
+        }
+    }
+}
 
-// void writeSurplusEvaluation(std::ofstream &out, const MoveEvaluation &eval, int depth)
-// {
-//     if (depth == DEPTH)
-//     {
-//         return;
-//     }
+void logMoveEvaluation(const MoveEvaluation &eval)
+{
+    std::ofstream out("log.txt");
+    if (!out.is_open())
+    {
+        std::cerr << "Failed to open log.txt" << std::endl;
+        return;
+    }
 
-//     if (eval.neededEvalCount != eval.evaluatedMoves)
-//     {
-//         out << eval.neededEvalCount << " / " << eval.evaluatedMoves << " / " << eval.totalEvalCount << "\n";
-//     }
+    writeMoveEvaluation(out, eval);
+    out.close();
+}
 
-//     if (not eval.listMoves.empty())
-//     {
-//         for (const auto &move : eval.listMoves)
-//         {
-//             writeSurplusEvaluation(out, move, depth + 1);
-//         }
-//     }
-// }
+void writeSurplusEvaluation(std::ofstream &out, const MoveEvaluation &eval, int depth)
+{
+    if (depth == get_depth_from_env())
+    {
+        return;
+    }
 
-// void logTooManyEvaluationsList(const MoveEvaluation &eval)
-// {
-//     std::ofstream out("surplusEval.txt");
-//     if (!out.is_open())
-//     {
-//         std::cerr << "Failed to open log.txt" << std::endl;
-//         return;
-//     }
+    if (eval.neededEvalCount != eval.evaluatedMoves)
+    {
+        out << eval.neededEvalCount << " / " << eval.evaluatedMoves << " / " << eval.totalEvalCount << "\n";
+    }
 
-//     writeSurplusEvaluation(out, eval);
-//     out.close();
-// }
+    if (not eval.listMoves.empty())
+    {
+        for (const auto &move : eval.listMoves)
+        {
+            writeSurplusEvaluation(out, move, depth + 1);
+        }
+    }
+}
 
-// std::pair<int, int> getBestMove(const MoveEvaluation &eval, bool maximizingPlayer)
-// {
-//     int bestScore = std::numeric_limits<int>::min();
-//     int worstScore = std::numeric_limits<int>::max();
-//     std::pair<int, int> bestMove = {-1, -1};
+void logTooManyEvaluationsList(const MoveEvaluation &eval)
+{
+    std::ofstream out("surplusEval.txt");
+    if (!out.is_open())
+    {
+        std::cerr << "Failed to open log.txt" << std::endl;
+        return;
+    }
 
-//     for (const auto &move : eval.listMoves)
-//     {
-//         if (maximizingPlayer)
-//         {
-//             if (move.score > bestScore)
-//             {
-//                 bestScore = move.score;
-//                 bestMove = move.move;
-//             }
-//         }
-//         else
-//         {
-//             if (move.score < worstScore)
-//             {
-//                 worstScore = move.score;
-//                 bestMove = move.move;
-//             }
-//         }
-//     }
+    writeSurplusEvaluation(out, eval);
+    out.close();
+}
 
-//     return bestMove;
-// }
+std::pair<int, int> getBestMove(const MoveEvaluation &eval, bool maximizingPlayer)
+{
+    int bestScore = std::numeric_limits<int>::min();
+    int worstScore = std::numeric_limits<int>::max();
+    std::pair<int, int> bestMove = {-1, -1};
+
+    for (const auto &move : eval.listMoves)
+    {
+        if (maximizingPlayer)
+        {
+            if (move.score > bestScore)
+            {
+                bestScore = move.score;
+                bestMove = move.move;
+            }
+        }
+        else
+        {
+            if (move.score < worstScore)
+            {
+                worstScore = move.score;
+                bestMove = move.move;
+            }
+        }
+    }
+
+    return bestMove;
+}
+
+std::ostream& operator<<(std::ostream& stream, std::vector<std::string> moves)
+{
+    for (const std::string& move : moves)
+    {
+        stream << move << ' ';
+    }
+    return stream;
+}
