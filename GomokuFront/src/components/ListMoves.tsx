ea49b25e1760daa60ddb3a3b89bdcd9a3f7f05e8
Antoine Labalette <labalette.antoine@gmail.com>
2024-04-24 15:47:34 +0200

    feat: make listMoves auto scrollable

--- GomokuFront/src/components/ListMoves.tsx
+++ GomokuFront/src/components/ListMoves.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const moveStyle = (isCurrentMove: boolean) => ({
   fontWeight: isCurrentMove ? "bold" : "normal",
@@ -13,11 +13,14 @@ interface IListMoves {
 
 const ListMoves: React.FC<IListMoves> = ({ moves, currentMove }) => {
   const formattedMoves = [];
+  const endOfListRef = useRef<HTMLTableRowElement>(null);
+
   for (let i = 0; i < moves.length; i += 1) {
     const firstMove = moves[i];
 
+    const ref = currentMove === i + 1 ? endOfListRef : null
     formattedMoves.push(
-      <tr key={i}>
+      <tr key={i} ref={ref}>
         <td className="text-left py-2" style={moveStyle(currentMove === i + 1)}>
           {i + 1}. {firstMove}
         </td>
@@ -25,13 +28,23 @@ const ListMoves: React.FC<IListMoves> = ({ moves, currentMove }) => {
     );
   }
 
+  useEffect(() => {
+    // Scrolls to the bottom of the list every time the 'moves' array changes
+    if (endOfListRef.current) {
+      endOfListRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [moves, currentMove]); // Only re-run the effect if 'moves' changes
+
   return (
     <div className="flex-grow overflow-y-auto shadow-blur custom-scrollbar">
       <table className="border-separate border-spacing-0 w-full bg-[rgba(10,23,38,0.8)]">
-        <tbody>{formattedMoves}</tbody>
+        <tbody>
+          {formattedMoves}
+        </tbody>
       </table>
     </div>
   );
 };
 
 export default ListMoves;
+
