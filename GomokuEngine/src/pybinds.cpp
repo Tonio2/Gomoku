0a773c87bb8b4f9048beb75ecce5df4094bb44a1
Antoine Labalette <labalette.antoine@gmail.com>
2024-04-25 12:23:46 +0200

    test

--- GomokuEngine/src/pybinds.cpp
+++ GomokuEngine/src/pybinds.cpp
@@ -1,11 +1,10 @@
-
+#include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include "ai/gomoku_ai.h"
 #include "engine/gomoku_engine.h"
 #include "engine/gomoku_pattern_reconizer.h"
 #include "room/game_room.h"
 #include "utils/gomoku_utilities.h"
-#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
