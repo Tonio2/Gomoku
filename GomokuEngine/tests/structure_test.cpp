359bf02a9e8ce45fd79c46aafb2f915fb8e99cc1
amasson <giantwow3896@gmail.com>
2024-05-10 21:02:49 +0200

    Structure unit tests

--- GomokuEngine/tests/structure_test.cpp
+++ GomokuEngine/tests/structure_test.cpp
@@ -2,7 +2,31 @@
 #include "engine/gomoku_engine.h"
 #include "utils/gomoku_utilities.h"
 
-std::vector<std::vector<Player>> generate_flanked_structures(std::vector<Player> s, StructureType type)
+static const std::vector<std::pair<std::vector<Player>, StructureType>> structures_list = {
+    {{E, O, X}, StructureType::ONE},
+    {{X, O, E}, StructureType::ONE},
+    {{E, O, E}, StructureType::OPEN_ONE},
+    {{E, O, O, X}, StructureType::TWO},
+    {{X, O, O, E}, StructureType::TWO},
+    {{E, O, O, E}, StructureType::OPEN_TWO},
+    {{E, O, O, O, X}, StructureType::THREE},
+    {{X, O, O, O, E}, StructureType::THREE},
+    {{E, O, O, O, E}, StructureType::OPEN_THREE},
+    {{E, O, E, O, O, X}, StructureType::THREE},
+    {{X, O, E, O, O, E}, StructureType::THREE},
+    {{E, O, E, O, O, E}, StructureType::OPEN_THREE},
+    {{E, O, O, E, O, X}, StructureType::THREE},
+    {{X, O, O, E, O, E}, StructureType::THREE},
+    {{E, O, O, E, O, E}, StructureType::OPEN_THREE},
+    {{E, O, O, O, O, X}, StructureType::FOUR},
+    {{X, O, O, O, O, E}, StructureType::FOUR},
+    {{E, O, O, O, O, E}, StructureType::OPEN_FOUR},
+    {{E, O, O, O, O, O, X}, StructureType::FIVE_OR_MORE},
+    {{X, O, O, O, O, O, E}, StructureType::FIVE_OR_MORE},
+    {{E, O, O, O, O, O, E}, StructureType::FIVE_OR_MORE},
+};
+
+std::vector<std::vector<Player>> generate_flanked_structures(std::vector<Player> structure, StructureType type)
 {
     std::vector<std::vector<Player>> ret = {};
     std::vector<Player> flanks = {X, E, O};
@@ -12,12 +36,12 @@ std::vector<std::vector<Player>> generate_flanked_structures(std::vector<Player>
         {
             bool push = true;
             std::vector<Player> flanked_struct = {flanks[i]};
-            flanked_struct.insert(flanked_struct.end(), s.begin(), s.end());
+            flanked_struct.insert(flanked_struct.end(), structure.begin(), structure.end());
             flanked_struct.push_back(flanks[j]);
 
             if (type == StructureType::TWO || type == StructureType::OPEN_TWO)
             {
-                if ((flanks[i] == O && s[0] == E) || (flanks[j] == O && s[s.size() - 1] == E))
+                if ((flanks[i] == O && structure[0] == E) || (flanks[j] == O && structure[structure.size() - 1] == E))
                 {
                     push = false;
                 }
@@ -31,99 +55,54 @@ std::vector<std::vector<Player>> generate_flanked_structures(std::vector<Player>
     return ret;
 }
 
-bool test_struct(std::vector<Player> s, StructureType expected_type)
+TEST(Structures, StructureAt)
 {
-    GomokuGame game(s.size(), 1);
-
-    for (int i = 0; i < s.size(); i++)
-        game.set_board_value(0, i, s[i]);
-
-    GomokuPatternReconizer reconizer(O);
-
-    reconizer.find_patterns_in_board(game);
-
-    GomokuCellIndex idx(0, 2);
-
-    StructureType type = reconizer.get_structure_at(idx, PatternDirection::LeftToRight).first;
-
-    if (type != expected_type)
+    for (const auto &[structure, expected_type] : structures_list)
     {
-        std::cout << "Found " << type << " but expected " << expected_type;
-        return false;
-    }
-
-    return true;
-}
-
-bool test_walled_struct(std::vector<Player> s, StructureType expected_type)
-{
-    GomokuGame game(s.size(), 1);
-
-    for (int i = 0; i < s.size(); i++)
-        game.set_board_value(0, i, s[i]);
-
-    GomokuPatternReconizer reconizer(O);
-
-    reconizer.find_patterns_in_board(game);
-
-    GomokuCellIndex idx(0, 1);
-
-    StructureType type = reconizer.get_structure_at(idx, PatternDirection::LeftToRight).first;
-
-    if (type != expected_type)
-    {
-        std::cout << "Found " << type << " but expected " << expected_type;
-        return false;
-    }
-
-    return true;
-}
-
-void test_get_structure_at()
-{
-
-    std::vector<std::pair<std::vector<Player>, StructureType>> structures = {
-        {{E, O, X}, StructureType::ONE},
-        {{X, O, E}, StructureType::ONE},
-        {{E, O, E}, StructureType::OPEN_ONE},
-        {{E, O, O, X}, StructureType::TWO},
-        {{X, O, O, E}, StructureType::TWO},
-        {{E, O, O, E}, StructureType::OPEN_TWO},
-        {{E, O, O, O, X}, StructureType::THREE},
-        {{X, O, O, O, E}, StructureType::THREE},
-        {{E, O, O, O, E}, StructureType::OPEN_THREE},
-        {{E, O, E, O, O, X}, StructureType::THREE},
-        {{X, O, E, O, O, E}, StructureType::THREE},
-        {{E, O, E, O, O, E}, StructureType::OPEN_THREE},
-        {{E, O, O, E, O, X}, StructureType::THREE},
-        {{X, O, O, E, O, E}, StructureType::THREE},
-        {{E, O, O, E, O, E}, StructureType::OPEN_THREE},
-        {{E, O, O, O, O, X}, StructureType::FOUR},
-        {{X, O, O, O, O, E}, StructureType::FOUR},
-        {{E, O, O, O, O, E}, StructureType::OPEN_FOUR},
-        {{E, O, O, O, O, O, X}, StructureType::FIVE_OR_MORE},
-        {{X, O, O, O, O, O, E}, StructureType::FIVE_OR_MORE},
-        {{E, O, O, O, O, O, E}, StructureType::FIVE_OR_MORE},
-    };
-
-    for (int i = 0; i < structures.size(); i++)
-    {
-        std::cout << "TEMPLATE : " << structures[i].first << " " << structures[i].second << std::endl;
-        std::vector<std::vector<Player>> flanked_structures = generate_flanked_structures(structures[i].first, structures[i].second);
+        std::vector<std::vector<Player>> flanked_structures = generate_flanked_structures(structure, expected_type);
         for (std::vector<Player> flanked_structure : flanked_structures)
         {
-            std::cout << flanked_structure << " : ";
-            if (test_struct(flanked_structure, structures[i].second))
-                std::cout << "OK";
-            std::cout << std::endl;
+            GomokuGame game(flanked_structure.size(), 1);
+
+            for (int i = 0; i < flanked_structure.size(); i++)
+                game.set_board_value(0, i, flanked_structure[i]);
+
+            GomokuPatternReconizer reconizer(O);
+
+            reconizer.find_patterns_in_board(game);
+
+            GomokuCellIndex idx(0, 2);
+
+            StructureType type = reconizer.get_structure_at(idx, PatternDirection::LeftToRight).first;
+
+            EXPECT_EQ(type, expected_type);
         }
-        std::cout << "|" << structures[i].first << "|" << " : ";
-        if (test_walled_struct(structures[i].first, structures[i].second))
-            std::cout << "OK";
-        std::cout << std::endl;
     }
 }
 
-TEST(Structures, AllCombinaison)
+TEST(Structures, WalledStructs)
 {
+    for (const auto &[structure, expected_type] : structures_list)
+    {
+        GomokuGame game(structure.size(), 1);
+
+        for (int i = 0; i < structure.size(); i++)
+            game.set_board_value(0, i, structure[i]);
+
+        GomokuPatternReconizer reconizer(O);
+
+        reconizer.find_patterns_in_board(game);
+
+        GomokuCellIndex idx(0, 1);
+
+        StructureType type = reconizer.get_structure_at(idx, PatternDirection::LeftToRight).first;
+
+        EXPECT_EQ(type, expected_type);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
