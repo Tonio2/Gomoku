c7f0467685dc55dbbbe7b221174bd5b9863c7f52
Antoine Labalette <labalette.antoine@gmail.com>
2024-05-15 12:11:28 +0200

    feat: ai vs ai in web client

--- GomokuBack/server_gomoku.py
+++ GomokuBack/server_gomoku.py
@@ -173,9 +173,10 @@ def create_room():
     rule_style = request.json.get("rule_style", 0)
     ai_player = request.json.get("ai_player", 2)
     ai_name = request.json.get("ai_name", "medium")
+    ai_name2 = request.json.get("ai_name2", "medium")
     room = rooms.get(user_id)
     if not room:
-        room = GomokuRoom(size, mode, rule_style, ai_player, ai_name)
+        room = GomokuRoom(size, mode, rule_style, ai_player, ai_name, ai_name2)
         rooms[user_id] = room
     state = room.get_state()
     state["success"] = True
