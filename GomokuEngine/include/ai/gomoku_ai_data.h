8207b9c46aa95d4867285ab26b9c128f82faca14
Antoine Labalette <labalette.antoine@gmail.com>
2024-05-18 13:36:51 +0200

    feat: test new heuristic (bad)

--- GomokuEngine/include/ai/gomoku_ai_data.h
+++ GomokuEngine/include/ai/gomoku_ai_data.h
@@ -6,32 +6,32 @@
 namespace AI::MinMaxV2
 {
 
-class GomokuAIData
-{
-public:
-    GomokuAIData();
-    GomokuAIData(const GomokuAIData &copy);
-    GomokuAIData &operator=(const GomokuAIData &copy);
-    virtual ~GomokuAIData();
+    class GomokuAIData
+    {
+    public:
+        GomokuAIData();
+        GomokuAIData(const GomokuAIData &copy);
+        GomokuAIData &operator=(const GomokuAIData &copy);
+        virtual ~GomokuAIData();
 
-    void save_to_file(std::string filename) const;
-    void load_from_file(std::string filename);
+        void save_to_file(std::string filename) const;
+        void load_from_file(std::string filename);
 
-    float value_of_structure(int structure_type) const;
-    float value_of_multiple_forced() const;
-    float value_of_multiple_o4() const;
-    float value_of_captures(int capture_count) const;
+        float value_of_structure(int structure_type) const;
+        float value_of_multiple_forced() const;
+        float value_of_multiple_o4() const;
+        float value_of_captures(int capture_count, int half_open_two_count) const;
 
-    static const int STC = StructureType::COUNT_STRUCTURE_TYPE;
-    static const int VALUES_COUNT = STC + 5;
-    float values[VALUES_COUNT];
+        static const int STC = StructureType::COUNT_STRUCTURE_TYPE;
+        static const int VALUES_COUNT = STC + 5;
+        float values[VALUES_COUNT];
 
-private:
-    friend class GomokuAIDataMutator;
+    private:
+        friend class GomokuAIDataMutator;
 
-    friend std::ostream &operator<<(std::ostream &stream, const GomokuAIData &ai_data);
-};
+        friend std::ostream &operator<<(std::ostream &stream, const GomokuAIData &ai_data);
+    };
 
-std::ostream &operator<<(std::ostream &stream, const GomokuAIData &ai_data);
+    std::ostream &operator<<(std::ostream &stream, const GomokuAIData &ai_data);
 
 } // namespace AI::MinMaxV2
