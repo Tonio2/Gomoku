7adcfcb2c6fc6d7ae9416549a9ea4a0e9f6d2631
Antoine Labalette <labalette.antoine@gmail.com>
2024-05-26 17:52:11 +0200

    fix: store relevant moves in MoveHeuristic for perfs

--- GomokuEngine/include/ai/gomoku_ai_minmaxv3.h
+++ GomokuEngine/include/ai/gomoku_ai_minmaxv3.h
@@ -3,19 +3,46 @@
 
 #include "gomoku_ai_interface.h"
 #include "gomoku_ai_data.h"
+#include <fstream>
 
 namespace AI::MinMaxV3
 {
+    struct MoveHeuristic
+    {
+        uint8_t row;
+        uint8_t col;
 
-    
+        int score;
+
+        bool operator==(const MoveHeuristic &other) const
+        {
+            return (row == other.row && col == other.col && score == other.score);
+        }
+    };
+
+    struct MoveEvaluation
+    {
+        int row;
+        int col;
+        std::vector<MoveEvaluation> listMoves = {};
+        int score = 0;
+        int cur_move_id = 0;
+        int best_move_id = 0;
+        int initial_score = 0;
+        std::vector<MoveHeuristic> relevant_moves = {};
+
+    };
 
     class GomokuAI : public IGomokuAI
     {
     public:
+        GomokuAI(const GomokuAiSettings &settings);
+        MoveEvaluation suggest_move_evaluation(const GomokuGame &board);
         Move suggest_move(const GomokuGame &board, int currentMove = 0) override;
         std::vector<Move> suggest_move_sequence(const GomokuGame &board) override;
+        
         int get_heuristic_evaluation(const GomokuGame &board, Player player) override;
-
+        const GomokuAIData &get_evaluation_data() const;
     private:
         GomokuGame game;
         int depth;  // min max depth algorithm
@@ -26,24 +53,23 @@ namespace AI::MinMaxV3
 
         std::vector<std::pair<int, int>> killer_moves;
 
-        void evaluateNode(const MoveHeuristic &move, int moveId, int _depth, MoveEvaluation &eval, int &alpha, int &beta, bool maximizingPlayer, int &extremEval, std::pair<int, int> &best_move, bool isFirstMove);
+        void evaluateNode(const MoveHeuristic &move, int move_id, int _depth, MoveEvaluation &eval, int &alpha, int &beta, bool maximizingPlayer, int &extremEval, std::pair<int, int> &best_move, bool isFirstMove);
 
-        void minimax(MoveEvaluation &eval, int _depth, int alpha, int beta, bool maximizingPlayer, int row, int col);
+        void minimax(MoveEvaluation &eval, int _depth, int alpha, int beta, bool maximizingPlayer);
         int score_player(Player player);
 
-        void find_relevant_moves(std::vector<MoveHeuristic> &out_relevant_moves, int _depth) const;
-        bool is_cell_relevant(int row, int col) const;
+        void compute_relevant_moves(std::vector<MoveHeuristic> &out_relevant_moves, int _depth) const;
 
         int _heuristic_evaluation();
-        void sortMoves(std::vector<MoveHeuristic> &moves, bool maximizingPlayer, MoveEvaluation &eval);
+        void sortMoves(MoveEvaluation &eval, bool maximizingPlayer);
+        
 
-    public:
-        GomokuAI(const GomokuAiSettings &settings);
-        MoveEvaluation suggest_move_evaluation(const GomokuGame &board);
-
-        const GomokuAIData &get_evaluation_data() const;
-
-        std::vector<MoveHeuristic> get_relevant_moves(const GomokuGame &board);
+        // std::vector<MoveHeuristic> get_relevant_moves(const GomokuGame &board);
     };
 
+    std::pair<int, int> getBestMove(const MoveEvaluation &eval, bool maximizingPlayer = true);
+    const MoveEvaluation &getBestMoveEvaluation(const MoveEvaluation &eval, bool maximizingPlayer = true);
+
+    void writeMoveEvaluation(std::ostream &out, const MoveEvaluation &eval, std::vector<std::vector<std::string>> &csvData, int depth = 0, int i = -1);
+    void logMoveEvaluation(const MoveEvaluation &eval, std::string filename);
 } // namespace AI::MinMaxV3
