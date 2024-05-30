25a8eac16159d38d6294e9f800eac31b935b60f3
amasson <giantwow3896@gmail.com>
2024-05-30 13:11:17 +0200

    Check capurable five by two hidden in gap three

--- GomokuEngine/src/engine/gomoku_pattern_reconizer.cpp
+++ GomokuEngine/src/engine/gomoku_pattern_reconizer.cpp
@@ -330,7 +330,8 @@ const std::vector<int> &GomokuPatternReconizer::get_pattern_count() const
 
 bool GomokuPatternReconizer::five_or_more_cant_be_captured(const GomokuGame &board)
 {
-    if (_cached_pattern_count[StructureType::TWO] <= 0)
+    if (get_pattern_count()[StructureType::TWO] <= 0
+        && get_pattern_count()[StructureType::THREE] <= 0)
         return true;
 
     // TODO: Check intersection of five or more patterns with blocked two
